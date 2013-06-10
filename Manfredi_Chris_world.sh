c1058bad374c77994c523a26d9ad61b033ebd44a
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-06-10 13:58:41 -0400

    Lab 3 World.sh file
    World.sh

---
+++ Manfredi_Chris_world.sh
@@ -0,0 +1,64 @@
+mkdir ~/Desktop/earth
+mkdir ~/Desktop/earth/asia
+mkdir ~/Desktop/earth/europe
+mkdir ~/Desktop/earth/north_america
+mkdir ~/Desktop/earth/south_america
+touch ~/Desktop/earth/asia/beijing.bak
+touch ~/Desktop/earth/asia/hong_kong
+touch ~/Desktop/earth/asia/seoul.txt
+touch ~/Desktop/earth/asia/tokyo.jpg
+touch ~/Desktop/earth/asia/ulaanbaatar.html
+touch ~/Desktop/earth/europe/barcelona
+touch ~/Desktop/earth/europe/london.jpg
+touch ~/Desktop/earth/europe/marseille.txt
+touch ~/Desktop/earth/europe/oslo.html
+touch ~/Desktop/earth/europe/rome.bak
+touch ~/Desktop/earth/north_america/new_york
+touch ~/Desktop/earth/north_america/toronto.jpg
+touch ~/Desktop/earth/north_america/cancouver.txt
+touch ~/Desktop/earth/north_america/orlando.html
+touch ~/Desktop/earth/north_america/sacramento.bak
+touch ~/Desktop/earth/south_america/buenos aires.txt
+touch ~/Desktop/earth/south_america/patagonia.bak
+touch ~/Desktop/earth/south_america/rio.jpg
+touch ~/Desktop/earth/south_america/san_salvador
+touch ~/Desktop/earth/south_america/montevideo.html
+
+#part 2:
+ 
+mkdir ~/Desktop/earth/asia/china
+mkdir ~/Desktop/earth/asia/japan
+mkdir ~/Desktop/earth/asia/korea
+mkdir ~/Desktop/earth/asia/mongolia
+mkdir ~/Desktop/earth/europe/france
+mkdir ~/Desktop/earth/europe/italy
+mkdir ~/Desktop/earth/europe/norway
+mkdir ~/Desktop/earth/europe/spain
+mkdir ~/Desktop/earth/europe/uk
+mkdir ~/Desktop/earth/north_america/us
+mkdir ~/Desktop/earth/north_america/canada
+mkdir ~/Desktop/earth/south_america/argentina
+mkdir ~/Desktop/earth/south_america/brazil
+mkdir ~/Desktop/earth/south_america/el_salvador
+mkdir ~/Desktop/earth/south_america/uruguay
+mv ~/Desktop/earth/asia/hong_kong ~/Desktop/earth/asia/china/hong_kong
+mv ~/Desktop/earth/asia/beijing.bak ~/Desktop/earth/asia/china/peiking.bak
+mv ~/Desktop/earth/asia/tokyo.jpg ~/Desktop/earth/asia/china/kyoto.jpg
+mv ~/Desktop/earth/asia/seoul.txt ~/Desktop/earth/asia/china/incheon.txt
+mv ~/Desktop/earth/asia/ulaanbaatar.html ~/Desktop/earth/asia/china/hovd.html
+mv ~/Desktop/earth/europe/marseille.txt ~/Desktop/earth/europe/france/paris.txt
+mv ~/Desktop/earth/europe/rome.bak ~/Desktop/earth/europe/italy/venice.bak
+mv ~/Desktop/earth/europe/olso.html ~/Desktop/earth/europe/bergen.html
+mv ~/Desktop/earth/europe/barcelona ~/Desktop/earth/europe/spain/barcelona
+mv ~/Desktop/earth/europe/london.jpg ~/Desktop/earth/europe/uk/manchester.jpg
+mv ~/Desktop/earth/north_america/sacramento.bak ~/Desktop/earth/north_america/us/cleveland.bak
+mv ~/Desktop/earth/north_america/new_york ~/Desktop/earth/north_america/us/new_york
+mv ~/Desktop/earth/north_america/orlando.html ~/Desktop/earth/north_america/us/orlando.mp3
+mv ~/Desktop/earth/north_america/vancouver.txt ~/Desktop/earth/north_america/canada/halifax.txt
+mv ~/Desktop/earth/north_america/toronto.jpg ~/Desktop/earth/north_america/canada/quebec.jpg
+mv ~/Desktop/earth/south_america/buenos aires.txt ~/Desktop/earth/south_america/argentina/cordoba.txt
+mv ~/Desktop/earth/south_america/patagonia.bak ~/Desktop/earth/south_america/argentina/san_juan.bak
+mv ~/Desktop/earth/south_america/rio.jpg ~/Desktop/earth/south_america/brazil/sao_paulo.jpg
+mv ~/Desktop/earth/south_america/san_salvador ~/Desktop/earth/south_america/el_salvador/san_salvador
+mv ~/Desktop/earth/south_america/montevideo.html ~/Desktop/earth/south_america/uruguay/maldonado.html
+
